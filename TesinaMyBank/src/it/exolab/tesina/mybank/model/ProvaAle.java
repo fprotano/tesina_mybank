be4997e4a58ba82e04a0dffabea88a7e396f638e
afren <afren@LAPTOP-88R6BNSD.fritz.box>
2021-05-27 15:23:00 +0200

    ciao belli

--- TesinaMyBank/src/it/exolab/tesina/mybank/model/ProvaAle.java
+++ TesinaMyBank/src/it/exolab/tesina/mybank/model/ProvaAle.java
@@ -1,5 +1,24 @@
 package it.exolab.tesina.mybank.model;
 
 public class ProvaAle {
+	private String lettera;
+	private int id;
+	public String getLettera() {
+		return lettera;
+	}
+	public void setLettera(String lettera) {
+		this.lettera = lettera;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	
+	
+	
+	
 
 }
