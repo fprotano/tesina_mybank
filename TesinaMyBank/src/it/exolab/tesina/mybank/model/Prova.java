e7a9a4b2c664d1fbd697fa2d778e585febfe62bd
luca- <luca-@LAPTOP-TFS01PTJ.fritz.box>
2021-05-27 15:19:47 +0200

    classe prova

---
+++ TesinaMyBank/src/it/exolab/tesina/mybank/model/Prova.java
@@ -0,0 +1,27 @@
+package it.exolab.tesina.mybank.model;
+
+public class Prova {
+	
+	private String ciao;
+	 
+	private int numero;
+
+	public String getCiao() {
+		return ciao;
+	}
+
+	public void setCiao(String ciao) {
+		this.ciao = ciao;
+	}
+
+	public int getNumero() {
+		return numero;
+	}
+
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+	
+	
+
+}
