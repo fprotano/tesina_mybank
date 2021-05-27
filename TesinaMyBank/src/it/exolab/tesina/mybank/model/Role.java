6c073b842f4dd6d7ba30fce5b246235880bdf573
Matteo Bertolin <mbertolin88@gmail.com>
2021-05-27 16:41:23 +0200

    Role + Staff models + aggiornamento account model

---
+++ TesinaMyBank/src/it/exolab/tesina/mybank/model/Role.java
@@ -0,0 +1,32 @@
+package it.exolab.tesina.mybank.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="role")
+public class Role {
+	@Id
+	private Integer id;
+	private String title;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	@Override
+	public String toString() {
+		return "Role [id=" + id + ", title=" + title + "]";
+	}
+	public Role() {
+		super();
+	}
+}
