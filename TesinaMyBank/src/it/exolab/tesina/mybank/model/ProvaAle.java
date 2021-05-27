bf7ad838e1894de97cdba494872d1701d179d352
afren <afren@LAPTOP-88R6BNSD.fritz.box>
2021-05-27 15:32:24 +0200

    ciao belli

--- TesinaMyBank/src/it/exolab/tesina/mybank/model/ProvaAle.java
+++ TesinaMyBank/src/it/exolab/tesina/mybank/model/ProvaAle.java
@@ -1,5 +1,15 @@
 package it.exolab.tesina.mybank.model;
 
 public class ProvaAle {
+	private int numero;
+
+	public int getNumero() {
+		return numero;
+	}
+
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+	
 
 }
