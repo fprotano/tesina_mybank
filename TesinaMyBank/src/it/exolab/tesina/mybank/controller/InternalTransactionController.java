8fa1ccc14342f4f768e850d3bbad258aa27cb910
matteolamagra98 <matteolamagra@gmail.com>
2021-05-28 12:21:45 +0200

    Internal transaction controller

--- TesinaMyBank/src/it/exolab/tesina/mybank/controller/InternalTransactionController.java
+++ TesinaMyBank/src/it/exolab/tesina/mybank/controller/InternalTransactionController.java
@@ -1,20 +1,92 @@
 package it.exolab.tesina.mybank.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import it.exolab.tesina.mybank.model.ExternalTransaction;
+import it.exolab.tesina.mybank.model.HTTPResponse;
+import it.exolab.tesina.mybank.model.InternalTransaction;
 import it.exolab.tesina.mybank.service.InternalTransactionService;
 
 @Controller
-@RequestMapping(value="internal_transaction")
+@RequestMapping(value = "internalTransaction")
 
 public class InternalTransactionController {
-	
+
 	private InternalTransactionService internalTransactionService;
 
-@Autowired(required=true)
-public void setExternalTransactionService(InternalTransactionService internalTransactionService) {
-	this.internalTransactionService = internalTransactionService;
-}
+	@Autowired(required = true)
+	public void setExternalTransactionService(InternalTransactionService internalTransactionService) {
+		this.internalTransactionService = internalTransactionService;
+	}
+
+	@RequestMapping(value = "insert", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseBody
+	public HTTPResponse register(@RequestBody InternalTransaction internalTransaction) {
+		HTTPResponse response = new HTTPResponse();
+		if (internalTransaction != null) {
+			this.internalTransactionService.insert(internalTransaction);
+			response.setData(internalTransaction);
+			response.setSuccess(true);
+			return response;
+		} else {
+			response.setSuccess(false);
+			response.setErr("Errore");
+			response.setErr_code("01");
+			return response;
+		}
+	}
+
+	@RequestMapping(value = "findOne", method = RequestMethod.GET, consumes = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseBody
+	public HTTPResponse findOne(@RequestBody Integer id) {
+		HTTPResponse response = new HTTPResponse();
+		if (id != null) {
+			this.internalTransactionService.find(id);
+			response.setData(id);
+			response.setSuccess(true);
+			return response;
+		} else {
+			response.setSuccess(false);
+			response.setErr("Errore");
+			response.setErr_code("01");
+			return response;
+		}
+	}
+
+	@RequestMapping(value = "findAll", method = RequestMethod.GET, consumes = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseBody
+	public HTTPResponse findAll() {
+		HTTPResponse response = new HTTPResponse();
+		List<InternalTransaction> transazioni = this.internalTransactionService.findAll();
+		response.setData(transazioni);
+		response.setSuccess(true);
+		return response;
+
+	}
+
+	@RequestMapping(value = "delete", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseBody
+	public HTTPResponse delete(@RequestBody Integer id) {
+		HTTPResponse response = new HTTPResponse();
+		if (id != null) {
+			this.internalTransactionService.delete(id);
+			response.setData(id);
+			response.setSuccess(true);
+			return response;
+		} else {
+			response.setSuccess(false);
+			response.setErr("Errore");
+			response.setErr_code("01");
+			return response;
+
+		}
+	}
 }
