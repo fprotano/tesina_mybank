5c8d52837a4414247c581409ff8e863b8108b2a1
luca- <luca-@LAPTOP-TFS01PTJ.fritz.box>
2021-05-28 11:01:02 +0200

    ExternalTransaction con HTTPRESPONSE

--- TesinaMyBank/src/it/exolab/tesina/mybank/controller/ExternalTransactionController.java
+++ TesinaMyBank/src/it/exolab/tesina/mybank/controller/ExternalTransactionController.java
@@ -1,5 +1,7 @@
 package it.exolab.tesina.mybank.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
@@ -77,5 +79,52 @@ public class ExternalTransactionController {
 					return response;
 		}
 	}
+	
+	@RequestMapping(value="findOne", method=RequestMethod.GET,consumes = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseBody
+	public HTTPResponse findOne(@RequestBody Integer id) {
+		HTTPResponse response = new HTTPResponse();
+		if(id!=null) {
+			this.externalTransactionService.find(id);
+			response.setData(id);
+			response.setSuccess(true);
+			return response;
+		} else {
+				response.setSuccess(false);
+				response.setErr("Errore");
+				response.setErr_code("01");
+					return response;
+		}
+	}
+	
+	@RequestMapping(value="findAll", method=RequestMethod.GET,consumes = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseBody
+	public HTTPResponse findAll() {
+			HTTPResponse response = new HTTPResponse();
+			List<ExternalTransaction> transazioni = this.externalTransactionService.findAll();
+			response.setData(transazioni);
+			response.setSuccess(true);
+			return response;
+		
+	}
+	
+	@RequestMapping(value="delete", method=RequestMethod.POST,consumes = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseBody
+	public HTTPResponse delete(@RequestBody Integer id) {
+		HTTPResponse response = new HTTPResponse();
+		if(id!=null) {
+			this.externalTransactionService.delete(id);
+			response.setData(id);
+			response.setSuccess(true);
+			return response;
+		} else {
+				response.setSuccess(false);
+				response.setErr("Errore");
+				response.setErr_code("01");
+					return response;
+		}
+	}
+	
+	
 
 }
