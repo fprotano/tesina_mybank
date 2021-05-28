3b30e11502a28d568f1fe321349abc13c9efe598
luca- <luca-@LAPTOP-TFS01PTJ.fritz.box>
2021-05-28 10:38:41 +0200

    External Controller inizio ed HTTPResponse

---
+++ TesinaMyBank/src/it/exolab/tesina/mybank/controller/ExternalTransactionController.java
@@ -0,0 +1,21 @@
+package it.exolab.tesina.mybank.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import it.exolab.tesina.mybank.service.ExternalTransactionService;
+
+@CrossOrigin
+@Controller
+@RequestMapping(value="libro")
+public class ExternalTransactionController {
+	
+	private ExternalTransactionService externalTransactionService;
+	
+	@Autowired(required=true)
+	public void setExternalTransactionService(ExternalTransactionService externalTransactionService) {
+		this.externalTransactionService = externalTransactionService;
+	}
+
+}
---
+++ TesinaMyBank/src/it/exolab/tesina/mybank/model/HTTPResponse.java
@@ -0,0 +1,59 @@
+package it.exolab.tesina.mybank.model;
+
+public class HTTPResponse {
+
+	
+	private boolean success;
+	private String err;
+	private String err_code;
+	private Object data;
+	private String tipo;
+	public HTTPResponse(boolean success, String err, String err_code, Object data) {
+		super();
+		this.success = success;
+		this.err = err;
+		this.err_code = err_code;
+		this.data = data;
+	}
+	
+	public HTTPResponse(Object data) {
+		this(true,null,null,data);
+	}
+	public HTTPResponse(String err,String err_code) {
+		this(true,err,err_code,null);
+	}
+	
+	
+	public HTTPResponse() {
+		super();
+	}
+
+	public boolean isSuccess() {
+		return success;
+	}
+	public void setSuccess(boolean success) {
+		this.success = success;
+	}
+	public String getErr() {
+		return err;
+	}
+	public void setErr(String err) {
+		this.err = err;
+	}
+	public String getErr_code() {
+		return err_code;
+	}
+	public void setErr_code(String err_code) {
+		this.err_code = err_code;
+	}
+	public Object getData() {
+		return data;
+	}
+	public void setData(Object data) {
+		this.data = data;
+	}
+	
+	
+	
+	
+}
