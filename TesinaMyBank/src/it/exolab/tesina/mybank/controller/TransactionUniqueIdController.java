502e4bef653da53399b951562c1b1eb2f1d37ae1
Matteo Bertolin <mbertolin88@gmail.com>
2021-05-28 16:11:26 +0200

    TransactionUniqueIdController aggiornato al DTO

--- TesinaMyBank/src/it/exolab/tesina/mybank/controller/TransactionUniqueIdController.java
+++ TesinaMyBank/src/it/exolab/tesina/mybank/controller/TransactionUniqueIdController.java
@@ -1,15 +1,17 @@
 package it.exolab.tesina.mybank.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-import it.exolab.tesina.mybank.model.Account;
-import it.exolab.tesina.mybank.model.TransactionUniqueId;
+import it.exolab.tesina.mybank.model.HTTPResponse;
+import it.exolab.tesina.mybank.model.dto.TransactionUniqueIdDTO;
 import it.exolab.tesina.mybank.service.TransactionUniqueIdService;
 
 @Controller
@@ -23,51 +25,67 @@ public class TransactionUniqueIdController {
 		this.transactionUniqueIdService = transactionUniqueIdService;
 	}
 	
-	@RequestMapping(value="list")
-	public ModelAndView list() {
-		ModelAndView ret = new ModelAndView("transactionUniqueId/list");
-		ret.addObject("items",this.transactionUniqueIdService.findAll());
-		return ret;
+	@RequestMapping(value="insert", method=RequestMethod.POST,consumes = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseBody
+	public HTTPResponse register(@RequestBody TransactionUniqueIdDTO transactionUniqueIdDto) {
+		HTTPResponse response = new HTTPResponse();
+		if(transactionUniqueIdDto!=null) {
+			this.transactionUniqueIdService.insert(transactionUniqueIdDto);
+			response.setData(transactionUniqueIdDto);
+			response.setSuccess(true);
+			return response;
+		} else {
+				response.setSuccess(false);
+				response.setErr("Errore");
+				response.setErr_code("01");
+					return response;
+		}
+	}
+	
+	@RequestMapping(value="findOne", method=RequestMethod.GET,consumes = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseBody
+	public HTTPResponse findOne(@RequestBody Integer id) {
+		HTTPResponse response = new HTTPResponse();
+		if(id!=null) {
+			this.transactionUniqueIdService.find(id);
+			response.setData(id);
+			response.setSuccess(true);
+			return response;
+		} else {
+				response.setSuccess(false);
+				response.setErr("Errore");
+				response.setErr_code("01");
+					return response;
+		}
+	}
+	
+	@RequestMapping(value="findAll", method=RequestMethod.GET,consumes = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseBody
+	public HTTPResponse findAll() {
+			HTTPResponse response = new HTTPResponse();
+			List<TransactionUniqueIdDTO> transazioni = this.transactionUniqueIdService.findAll();
+			response.setData(transazioni);
+			response.setSuccess(true);
+			return response;
 		
 	}
 	
-	@RequestMapping(value="add")
-	public ModelAndView add() {
-		ModelAndView ret = new ModelAndView("transactionUniqueId/add");
-		ret.addObject("transactionUniqueId", new TransactionUniqueId());
-		return ret;
-		
-	}
-	@RequestMapping(value="add", method=RequestMethod.POST)
-	public String insert(@ModelAttribute TransactionUniqueId transactionUniqueId) {
-		transactionUniqueIdService.insert(transactionUniqueId);
-		return "redirect:/transactionUniqueId/list";
-		
-	}
-	
-	
-	
-
-	@RequestMapping(value="edit/{id}")
-	public ModelAndView edit(@PathVariable int id) {
-		ModelAndView ret = new ModelAndView("transactionUniqueId/edit");
-		ret.addObject("transactionUniqueId", this.transactionUniqueIdService.find(id));
-		
-		return ret;
-		
-	}
-	@RequestMapping(value="edit/{id}", method=RequestMethod.POST)
-	public String update(@ModelAttribute TransactionUniqueId transactionUniqueId) {
-		transactionUniqueIdService.update(transactionUniqueId);
-		return "redirect:/transactionUniqueId/list";
-		
-	}
-	
-	@RequestMapping(value="delete/{id}")
-	public String delete(@PathVariable int id) {
-		transactionUniqueIdService.delete(id);
-		return "redirect:/transactionUniqueId/list";
-		 
+	@RequestMapping(value="delete", method=RequestMethod.POST,consumes = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseBody
+	public HTTPResponse delete(@RequestBody Integer id) {
+		HTTPResponse response = new HTTPResponse();
+		if(id!=null) {
+			this.transactionUniqueIdService.delete(id);
+			response.setData(id);
+			response.setSuccess(true);
+			return response;
+		} else {
+				response.setSuccess(false);
+				response.setErr("Errore");
+				response.setErr_code("01");
+					return response;
+					
+		}
 	}
 	
 }
