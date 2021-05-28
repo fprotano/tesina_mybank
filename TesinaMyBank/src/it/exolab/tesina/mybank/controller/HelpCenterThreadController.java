280065fb09c79a66bafff843cc7b24902f8e3bb8
matteolamagra98 <matteolamagra@gmail.com>
2021-05-28 12:52:31 +0200

    Help center thread : Controller, Repository, Service

---
+++ TesinaMyBank/src/it/exolab/tesina/mybank/controller/HelpCenterThreadController.java
@@ -0,0 +1,100 @@
+package it.exolab.tesina.mybank.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import it.exolab.tesina.mybank.model.HTTPResponse;
+import it.exolab.tesina.mybank.model.HelpCenter;
+import it.exolab.tesina.mybank.model.HelpCenterThread;
+import it.exolab.tesina.mybank.service.HelpCenterService;
+import it.exolab.tesina.mybank.service.HelpCenterThreadService;
+
+@Controller
+@RequestMapping(value="helpCenterThread")
+public class HelpCenterThreadController {
+	
+	private HelpCenterThreadService helpCenterThreadService;
+	
+	@Autowired(required=true)
+	public void setHelpCenterService(HelpCenterThreadService helpCenterThreadService) {
+		this.helpCenterThreadService = helpCenterThreadService;
+	}
+	
+	@RequestMapping(value = "insert", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseBody
+	public HTTPResponse register(@RequestBody HelpCenterThread helpCenterThread) {
+		HTTPResponse response = new HTTPResponse();
+		if (helpCenterThread != null) {
+			this.helpCenterThreadService.insert(helpCenterThread);
+			response.setData(helpCenterThread);
+			response.setSuccess(true);
+			return response;
+		} else {
+			response.setSuccess(false);
+			response.setErr("Errore");
+			response.setErr_code("01");
+			return response;
+		}
+	} 
+	
+	
+	@RequestMapping(value = "findOne", method = RequestMethod.GET, consumes = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseBody
+	public HTTPResponse findOne(@RequestBody Integer id) {
+		HTTPResponse response = new HTTPResponse();
+		if (id != null) {
+			this.helpCenterThreadService.find(id);
+			response.setData(id);
+			response.setSuccess(true);
+			return response;
+		} else {
+			response.setSuccess(false);
+			response.setErr("Errore");
+			response.setErr_code("01");
+			return response;
+		}
+	}
+	
+	
+	@RequestMapping(value="findAll", method=RequestMethod.GET,consumes = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseBody
+	public HTTPResponse findAll() {
+			HTTPResponse response = new HTTPResponse();
+			List<HelpCenterThread> transazioni = this.helpCenterThreadService.findAll();
+			response.setData(transazioni);
+			response.setSuccess(true);
+			return response;
+		
+	}
+	
+	
+	@RequestMapping(value="delete", method=RequestMethod.POST,consumes = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseBody
+	public HTTPResponse delete(@RequestBody Integer id) {
+		HTTPResponse response = new HTTPResponse();
+		if(id!=null) {
+			this.helpCenterThreadService.delete(id);
+			response.setData(id);
+			response.setSuccess(true);
+			return response;
+		} else {
+				response.setSuccess(false);
+				response.setErr("Errore");
+				response.setErr_code("01");
+					return response;
+					
+		}
+	}
+
+	
+	
+
+
+}
---
+++ TesinaMyBank/src/it/exolab/tesina/mybank/service/HelpCenterThreadService.java
@@ -0,0 +1,37 @@
+package it.exolab.tesina.mybank.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import it.exolab.tesina.mybank.model.HelpCenterThread;
+import it.exolab.tesina.mybank.repository.HelpCenterThreadRepository;
+
+public class HelpCenterThreadService {
+	
+	private HelpCenterThreadRepository helpCenterThreadRepository;
+
+	@Autowired(required=true)
+	public void setHelpCenterThreadRepository(HelpCenterThreadRepository helpCenterThreadRepository) {
+		this.helpCenterThreadRepository = helpCenterThreadRepository;
+	}
+	
+	public List<HelpCenterThread> findAll(){
+		return (List<HelpCenterThread>) helpCenterThreadRepository.findAll();
+	}
+	public void insert(HelpCenterThread internalTransaction){
+		this.helpCenterThreadRepository.save(internalTransaction);
+	}
+	
+	public void delete(Integer id){
+		this.helpCenterThreadRepository.delete(id);
+	}
+	public void update(HelpCenterThread helpCenterThread) {
+		helpCenterThreadRepository.save(helpCenterThread);
+	}
+	
+	public HelpCenterThread find(Integer id){
+		return helpCenterThreadRepository.findOne(id);
+	}
+
+}
