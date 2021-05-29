0ec6d0c2a4367f8530b9ada2066ef3d471dd9318
Matteo Bertolin <mbertolin88@gmail.com>
2021-05-29 19:10:47 +0200

    rolecontroller + findone

--- TesinaMyBank/src/it/exolab/tesina/mybank/controller/RoleController.java
+++ TesinaMyBank/src/it/exolab/tesina/mybank/controller/RoleController.java
@@ -1,12 +1,44 @@
 package it.exolab.tesina.mybank.controller;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
 
-@Entity
-@Table(name="role")
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import it.exolab.tesina.mybank.model.HTTPResponse;
+import it.exolab.tesina.mybank.service.RoleService;
+
+@Controller
+@RequestMapping(value="role")
 public class RoleController {
 	
-	 
+	private RoleService roleService;
+	
+	@Autowired
+	public void setRoleService(RoleService roleService) {
+		this.roleService = roleService;
+	}
+
+	@RequestMapping(value="findOne", method=RequestMethod.GET,consumes = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseBody
+	public HTTPResponse findOne(@RequestBody Integer id) {
+		HTTPResponse response = new HTTPResponse();
+		if(id!=null) {
+			this.roleService.find(id);
+			response.setData(id);
+			response.setSuccess(true);
+			return response;
+		} else {
+				response.setSuccess(false);
+				response.setErr("Errore");
+				response.setErr_code("01");
+					return response;
+		}
+	}
 	
 }
