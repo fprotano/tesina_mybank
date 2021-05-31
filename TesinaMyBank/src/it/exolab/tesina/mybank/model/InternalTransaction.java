6066085a6de1993b625216d1599ba07b752f7247
Matteo Bertolin <mbertolin88@gmail.com>
2021-05-31 12:25:28 +0200

    account + internaltransaction onetomany

--- TesinaMyBank/src/it/exolab/tesina/mybank/model/InternalTransaction.java
+++ TesinaMyBank/src/it/exolab/tesina/mybank/model/InternalTransaction.java
@@ -4,10 +4,17 @@ import java.security.Timestamp;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
+
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
 
 @Entity
 @Table(name="internal_transaction")
@@ -16,20 +23,36 @@ public class InternalTransaction {
 	@Id  
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
+	
 	@Column(name="created_at")
 	private Timestamp createdAt;
+	
 	@Column(name="custom_code")
 	private String customCode;
+	
 	@Column(name="transaction_id")
 	private String transactionId;
+	@Fetch(value=FetchMode.JOIN)
+	@OneToOne(fetch=FetchType.EAGER,optional=false)
+	@JoinColumn(name="transaction_id", nullable=false,insertable=false, updatable=false)
 	private TransactionUniqueId transactionUniqueId;
+	
+	@Column(name="amount")
 	private double amount;
+	
 	@Column(name="from_account_id")
 	private Integer fromAccountId ;
-	private Account account; 
+	@Fetch(value=FetchMode.JOIN)
+    @ManyToOne(fetch=FetchType.EAGER,optional=false)
+    @JoinColumn(name="to_account_id",nullable=false,insertable=false,updatable=false)
+	private Account accountFrom;
 	
 	@Column(name="to_account_id")
-	private Integer toAccountId; 
+	private Integer toAccountId;
+	@Fetch(value=FetchMode.JOIN)
+    @ManyToOne(fetch=FetchType.EAGER,optional=false)
+    @JoinColumn(name="to_account_id",nullable=false,insertable=false,updatable=false)
+	private Account accountTo;
 	
 	public Integer getId() {
 		return id;
