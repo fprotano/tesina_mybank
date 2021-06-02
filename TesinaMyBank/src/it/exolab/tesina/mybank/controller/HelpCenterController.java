260e11541f2ec0094dd148372078ef34a6d578bd
matteolamagra98 <matteolamagra@gmail.com>
2021-06-02 17:54:23 +0200

    help center controller

--- TesinaMyBank/src/it/exolab/tesina/mybank/controller/HelpCenterController.java
+++ TesinaMyBank/src/it/exolab/tesina/mybank/controller/HelpCenterController.java
@@ -1,14 +1,18 @@
 package it.exolab.tesina.mybank.controller;
 
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
 import java.util.List; 
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import it.exolab.tesina.mybank.model.HTTPResponse;
 import it.exolab.tesina.mybank.model.HelpCenter;
@@ -35,11 +39,15 @@ public class HelpCenterController {
 		this.helpCenterThreadService = helpCenterThreadService;
 	}
 	 
-	@RequestMapping(value = "insert", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
+	@RequestMapping(value = "insert/{id}", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
 	@ResponseBody
-	public HTTPResponse register(@RequestBody HelpCenter helpCenter) {
+	public HTTPResponse register(@RequestBody HelpCenter helpCenter, @PathVariable int id) {
 		HTTPResponse response = new HTTPResponse();
 		if (helpCenter != null) {
+			
+			LocalDateTime dataNow = LocalDateTime.now();
+			helpCenter.setFromAccountId(id); //id dell'utente
+			helpCenter.setCreatedAt(Timestamp.valueOf(dataNow));
 			this.helpCenterService.insert(helpCenter);
 			response.setData(helpCenter);
 			response.setSuccess(true);
