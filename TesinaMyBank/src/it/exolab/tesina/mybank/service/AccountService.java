88711136db6e01b0b92d4ac01bb8235bd7561c45
afren <afren@LAPTOP-88R6BNSD.fritz.box>
2021-06-03 15:30:24 +0200

    Validazioni account

--- TesinaMyBank/src/it/exolab/tesina/mybank/service/AccountService.java
+++ TesinaMyBank/src/it/exolab/tesina/mybank/service/AccountService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import it.exolab.tesina.mybank.exception.InvalidEmail;
 import it.exolab.tesina.mybank.exception.InvalidPassword;
 import it.exolab.tesina.mybank.exception.MaxLengthError;
+import it.exolab.tesina.mybank.exception.MinLengthError;
 import it.exolab.tesina.mybank.exception.RequiredFieldError;
 import it.exolab.tesina.mybank.model.Account;
 import it.exolab.tesina.mybank.model.dto.AccountDTO;
@@ -29,7 +30,8 @@ public class AccountService {
 	public List<Account> findAll(){
 		return (List<Account>)this.accountRepository.findAll();
 	}
-	public void insert(Account model) {
+	public void insert(Account model) throws RequiredFieldError, MaxLengthError, MinLengthError, InvalidEmail {
+		validateInsert(model);
 		this.accountRepository.save(model);
 	}
 	public void update(Account model) {
@@ -45,6 +47,25 @@ public class AccountService {
 		return this.accountRepository.findByEmailAndPassword(email, password);
 	}
 	
+	public void validateInsert(Account model) throws RequiredFieldError, MaxLengthError, MinLengthError, InvalidEmail {
+		Validator.required("email", model.getEmail());
+		Validator.required("password", model.getPassword());
+		Validator.required("name", model.getName());
+		Validator.required("surname", model.getSurname());
+		Validator.maxLength("email", model.getEmail(), 20);
+		Validator.maxLength("password", model.getPassword(), 20);
+		Validator.maxLength("name", model.getName(), 20);
+		Validator.maxLength("surname", model.getSurname(), 20);
+		Validator.minLength("email", model.getEmail(), 1);
+		Validator.minLength("password", model.getPassword(), 1);
+		Validator.minLength("name", model.getName(), 1);
+		Validator.minLength("surname", model.getSurname(), 1);
+		Validator.validateEmail("email", model.getEmail());
+		Validator.validateEmail("password", model.getPassword());
+		
+		
+		
+	}
 
 	
 }
