f59098d6cbed7957281badc2b7e53cd11ba4e81f
luca- <luca-@LAPTOP-TFS01PTJ.fritz.box>
2021-06-11 10:35:58 +0200

    ciao

---
+++ TesinaMyBank/src/it/exolab/tesina/mybank/factory/TransactionUniqueIdFactory.java
@@ -0,0 +1,16 @@
+package it.exolab.tesina.mybank.factory;
+
+public class TransactionUniqueIdFactory {
+
+	
+	public String TransactionUniqueId() {
+		StringBuilder transactionUniqueId = new StringBuilder();
+		String[] alfabeto = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
+		for(int i=0; i<=255; i++ ){
+			int numero = (int) (Math.random()*(34)) + 1;
+			String lettera = alfabeto[numero];
+			transactionUniqueId.append(lettera);
+		}
+		return TransactionUniqueId().toString();
+	}
+}
