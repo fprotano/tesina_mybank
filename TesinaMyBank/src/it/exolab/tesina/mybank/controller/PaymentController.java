ef87e78c0b1ecee0dd78ed6d8ece9caed4f4da4a
luca- <luca-@LAPTOP-TFS01PTJ.fritz.box>
2021-06-11 12:52:59 +0200

    payment

--- TesinaMyBank/src/it/exolab/tesina/mybank/controller/PaymentController.java
+++ TesinaMyBank/src/it/exolab/tesina/mybank/controller/PaymentController.java
@@ -1,12 +1,28 @@
 package it.exolab.tesina.mybank.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import it.exolab.tesina.mybank.model.Payment;
 
 @CrossOrigin
 @Controller
-@RequestMapping(value = "account")
+@RequestMapping(value = "payment")
 public class PaymentController {
 
+	
+	@RequestMapping(value = "inserisci", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseBody
+	public String pay(@RequestBody Payment payment, HttpSession session) {
+		session.setAttribute("payment", payment);
+		return "account/login";
+
+	}
 }
