61d12baf945a90ab78ca56a1280841c5ad61beaf
luca- <luca-@LAPTOP-TFS01PTJ.fritz.box>
2021-06-14 14:23:52 +0200

    ciao

--- TesinaMyBank/src/it/exolab/tesina/mybank/controller/PaymentController.java
+++ TesinaMyBank/src/it/exolab/tesina/mybank/controller/PaymentController.java
@@ -1,5 +1,8 @@
 package it.exolab.tesina.mybank.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -7,14 +10,33 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import it.exolab.tesina.mybank.model.Payment;
+import it.exolab.tesina.mybank.service.AccountService;
+import it.exolab.tesina.mybank.service.PaymentService;
+
 @CrossOrigin
 @Controller
 @RequestMapping(value = "payment")
 public class PaymentController {
+	
+	PaymentService paymentService;
+	
+	@Autowired
+	public void setPaymentService(PaymentService paymentService) {
+		this.paymentService = paymentService;
+	}
 
 	
 	@RequestMapping(value = "inserisci", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
-	public String pay(@RequestBody String model) {
+	public String pay(@RequestBody String model, HttpServletRequest request) {
+		Payment payment = new Payment();
+		payment.setEmail(request.getParameter("mail"));
+		payment.setAmount(Double.valueOf(request.getParameter("amount")));
+		payment.setCustomCode(request.getParameter("customCode"));
+		payment.setUrlSuccess(request.getParameter("urlSuccess"));
+		payment.setUrlUnDo(request.getParameter("urlUnDo"));
+		payment.setUrlNotify(request.getParameter("urlNotify"));
+		paymentService.insert(payment);
 		System.out.println("Sono arrivato nel metodo della banca ");
 		System.out.println(model);
 		return "redirect:/account/login";
--- TesinaMyBank/src/it/exolab/tesina/mybank/model/Payment.java
+++ TesinaMyBank/src/it/exolab/tesina/mybank/model/Payment.java
@@ -69,6 +69,9 @@ public class Payment {
 				+ ", transactionId=" + transactionId + ", urlUnDo=" + urlUnDo + ", urlSuccess=" + urlSuccess
 				+ ", urlNotify=" + urlNotify + "]";
 	}
+	public Payment() {
+		super();
+	}
 	
 	
 	
