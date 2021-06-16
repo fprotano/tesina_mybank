f0f4e790f0c80e5453aec4c31bc8a585510aa9df
luca- <luca-@LAPTOP-TFS01PTJ.fritz.box>
2021-06-16 14:23:42 +0200

    payment controller

--- TesinaMyBank/src/it/exolab/tesina/mybank/controller/PaymentController.java
+++ TesinaMyBank/src/it/exolab/tesina/mybank/controller/PaymentController.java
@@ -1,9 +1,16 @@
 package it.exolab.tesina.mybank.controller;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +22,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.google.gson.Gson;
 
 import it.exolab.tesina.mybank.exception.EntityNotFoundError;
 import it.exolab.tesina.mybank.exception.GenericError;
@@ -69,6 +78,58 @@ public class PaymentController {
 		return "redirect: http://localhost:4201/init-payment/"+payment.getId();
 		
 
+	}
+	
+//	@RequestMapping(value = "paymentNotify", method = RequestMethod.POST,consumes = MediaType.ALL_VALUE)
+//	@ResponseBody
+//	public void doPaymentNotify(@ModelAttribute OotlBank ootlBank) {
+//		
+//		AuctionOrderTransactionLog aotl = new AuctionOrderTransactionLog();
+//		Field[] fields = aotl.getClass().getDeclaredFields();
+//		System.out.println(fields);
+//		for(int i=0; i<ootlBank.getPn().length; i++) {
+//			//if() comparazione nome con parameter
+//			
+//		}
+//	}
+	
+
+	@RequestMapping(value = "sendData", method = RequestMethod.POST,consumes = MediaType.ALL_VALUE)
+	@ResponseBody
+	public void doAuctionOrderPayment(@RequestBody Payment model, HttpServletResponse httpServletResponse) throws IOException {
+		
+		System.out.println("nel doAuctionOrderPayment, Payment > " + model);
+		String projectUrl = model.getUrlSuccess();
+
+		Gson gson = new Gson();
+		String json = gson.toJson(model);
+	
+		System.out.println(json);
+		URL url = new URL(projectUrl);
+		HttpURLConnection connection = (HttpURLConnection) url.openConnection(); 
+		connection.setRequestMethod("POST");
+		connection.setDoOutput(true);
+
+		
+		connection.setRequestProperty("Accept-Charset", "UTF-8");
+		connection.setRequestProperty("Accept", "application/json" );
+		connection.setRequestProperty("Content-Type", "application/json" + "UTF-8");
+
+	    OutputStreamWriter writer = new OutputStreamWriter(connection.getOutputStream());
+
+	    writer.write(json);
+	    writer.flush();
+	    String line;
+
+	    BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+	    while ((line = reader.readLine()) != null) {
+	      System.out.println(line);
+	    }
+	    writer.close();
+	    reader.close();
+		connection.connect();
+
+		System.out.println("prima del ret > " + connection);
 	}
 	
 	@RequestMapping(value = "fillPayment", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
