a84dff8688d34829eb503cc779b36b9a06e6a40a
luca- <luca-@LAPTOP-TFS01PTJ.fritz.box>
2021-06-16 16:44:56 +0200

    mmmm

--- TesinaMyBank/src/it/exolab/tesina/mybank/controller/PaymentController.java
+++ TesinaMyBank/src/it/exolab/tesina/mybank/controller/PaymentController.java
@@ -101,6 +101,7 @@ public class PaymentController {
 		data.concat("PV[4]=customCode&PV[4]=" + model.getCustomCode());
 		
 		pushservice.notifyTransaction(model.getUrlNotify(), data);
+		
 	}
 	
 	@RequestMapping(value = "fillPayment", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
